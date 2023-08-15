b2809ee2120b8478b5ab9044e05c2bf539af3e6a
kasaisuhai <125857134+kasaisuhai@users.noreply.github.com>
2023-08-15 17:48:20 -0600

    added color theme functionality, reworked mobile nav

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 const lightMode = "css/light_mode.css";
 const darkMode = "css/dark_mode.css";
+const blueThemeStyle = "css/theme_blue.css";
+const greenThemeStyle = "css/theme_green.css";
+const orangeThemeStyle = "css/theme_orange.css";
+const purpleThemeStyle = "css/theme_purple.css";
+const swatchOrder = ["light", "dark", "accent", "medium", "highlight"];
 
 //Functions
 function initializePage(){
-    if(localStorage.getItem("darkChecked") === null){
+    if(localStorage.getItem("Initialized") === null){
         localStorage.setItem("darkChecked", false);
+        localStorage.setItem("currentTheme", "blue");
+
+        localStorage.setItem("Initialized", true);
         console.log("Initialized");
     }
     else{
-        let darkOn = Boolean(localStorage.getItem("darkChecked"));
+        let darkOn = localStorage.getItem("darkChecked");
+        let currentTheme = localStorage.getItem("currentTheme");
         console.log("Dark Mode Checked: " + darkOn);
+        console.log("Current Theme: " + currentTheme);
 
         if(darkOn === "true"){
             $(".form-check-input").prop("checked", true);
@@ -21,8 +31,39 @@ function initializePage(){
             $(".mode").attr("href", lightMode);
             console.log("Turned on Light Mode");
         }
+
+        setSelectedTheme(currentTheme);
         console.log("Already Initialized");
     }
+}
+function setSelectedTheme(color){
+    console.log("Setting Theme: " + color);
+    let themeSwatch = $("#selectedTheme .theme-swatch");
+
+    for(let i=0; i<themeSwatch.length; i++){
+        let newThemeClass = swatchOrder[i] + "-" + color;
+
+        themeSwatch.eq(i).removeClass();
+        themeSwatch.eq(i).addClass("theme-swatch " + newThemeClass);
+        console.log("Swatch: " + newThemeClass)
+    }
+
+    switch(color){
+        case "blue":
+            $("#themeStyle").attr("href", blueThemeStyle);
+            break;
+        case "green":
+            $("#themeStyle").attr("href", greenThemeStyle);
+            break;
+        case "orange":
+            $("#themeStyle").attr("href", orangeThemeStyle);
+            break;
+        case "purple":
+            $("#themeStyle").attr("href", purpleThemeStyle);
+            break;
+    }
+
+    localStorage.setItem("currentTheme", color);
 }
 
 //Event Listeners
@@ -38,4 +79,30 @@ $(".form-check-input").on("change", function(){
         $(".mode").attr("href", lightMode);
     }
     console.log("Switch On: " + switchOn);
+});
+
+$("#dropdownEmail").on("click", function(){
+    $("#reminderMethodDropdownButton").html("Email");
+    console.log("Email Selected");
+});
+$("#dropdownSms").on("click", function(){
+    $("#reminderMethodDropdownButton").html("SMS");
+    console.log("SMS Selected");
+});
+$("#dropdownBrowser").on("click", function(){
+    $("#reminderMethodDropdownButton").html("Browser");
+    console.log("Browser Selected");
+});
+
+$("#blueThemeDropdownItem").on("click", function(){
+    setSelectedTheme("blue");
+});
+$("#greenThemeDropdownItem").on("click", function(){
+    setSelectedTheme("green");
+});
+$("#orangeThemeDropdownItem").on("click", function(){
+    setSelectedTheme("orange");
+});
+$("#purpleThemeDropdownItem").on("click", function(){
+    setSelectedTheme("purple");
 });
